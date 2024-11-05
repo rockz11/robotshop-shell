9af8df6b9324678bdfea6b66e73b5ef8ba1738b3
Prughvi <prudhviraj3525@gmail.com>
2024-11-05 19:35:58 +0530

    first commit mongodb

--- 01-frontend/01-frontend.sh
+++ 01-frontend/01-frontend.sh
@@ -1,11 +1,16 @@
 dnf module disable nginx -y
 dnf module enable nginx:1.24 -y
 dnf install nginx -y
+
+cp nginx.conf /etc/nginx/nginx.conf
+
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+cd /usr/share/share/nginx/html
+unzip /tmp/frontend.zip
+
 systemctl enable nginx
 systemctl start nginx
-rm -rf /usr/share/nginx/html/* curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
-cd /usr/share/share/nginx/html
 
-unzip /tmp/frontend.zip
-vim /etc/nginx/nginx.conf
+
 systemctl restart nginx
