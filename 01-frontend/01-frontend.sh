06bbcc9ae7ebb9eed6171684f2165406c8a2a1ad
Prughvi <prudhviraj3525@gmail.com>
2024-11-27 20:09:19 +0530

    m

--- 01-frontend/01-frontend.sh
+++ 01-frontend/01-frontend.sh
@@ -6,11 +6,8 @@ cp nginx.conf /etc/nginx/nginx.conf
 
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
-
+cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-systemctl enable nginx
-systemctl start nginx
-
-
 systemctl restart nginx
+systemctl enable nginx
--- 02-Mongodb/mongodb.sh
+++ 02-Mongodb/mongodb.sh
@@ -1,8 +1,5 @@
 cp mongodb.repo /etc/yum.repos.d/mongo.repo
 
-sed -i -e 's/127.0.0.0.1/0.0.0.0/' /etc/mongod.conf
-
 dnf install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
-
