a07779b22305ede0d2b4f23268d514ddf2ac0d31
Prughvi <prudhviraj3525@gmail.com>
2024-11-28 16:26:34 +0530

    n

--- 02-Mongodb/mongodb.sh
+++ 02-Mongodb/mongodb.sh
@@ -1,5 +1,4 @@
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo
-
 dnf install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
--- catalogue/catalogue.sh
+++ catalogue/catalogue.sh
@@ -1,1 +1,20 @@
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo
+cp catalogue.service /etc/systemd/system/catalogue.service
 
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+
+useradd roboshop
+
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifact.s3.amazonaws.com/catalogue-v3.zip
+cd /app
+npm install
+
+dnf install mongodb-mongosh -y
+mongosh --host mongodb.devops11.online </app/db/master-data.js
+
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
